db4a68b2c654508d474b72b3ea27eb25e0aab4ef
manikjain <manik.jain@live.in>
2024-07-26 16:08:07 +0200

    update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "tf-remote-state20240726105442353600000001"
+    bucket         = "tf-remote-state20240726140424178200000001"
     key            = "development/terraform.tfstate"
     region         = "eu-central-1"
     encrypt        = true
-    kms_key_id     = "1935471b-c13e-4787-8b3e-72442731aa90"
+    kms_key_id     = "422ae280-c195-4c4e-9cfb-7e4ce2f63a3d"
     dynamodb_table = "tf-remote-state-lock"
   }
 }
