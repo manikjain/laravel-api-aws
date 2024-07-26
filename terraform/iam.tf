b19dd385867f6f1c19d80d85081617a072f7ee14
manikjain <manik.jain@live.in>
2024-07-26 20:47:00 +0200

    trigger test deployment to apprunner

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -2,11 +2,6 @@ data "aws_iam_policy" "apprunnerecr" {
   arn = "arn:aws:iam::aws:policy/service-role/AWSAppRunnerServicePolicyForECRAccess"
 }
 
-# import {
-#     to = aws_iam_role.apprunnerecr
-#     id = "AppRunnerECRAccessRole"
-# }
-
 resource "aws_iam_role" "apprunnerecr" {
     name = "AppRunnerECRAccessRoleNew"
     assume_role_policy = jsonencode({
@@ -24,8 +19,3 @@ resource "aws_iam_role" "apprunnerecr" {
     })
     managed_policy_arns = [data.aws_iam_policy.apprunnerecr.arn]
 }
-
-# resource "aws_iam_role_policy_attachment" "sto-readonly-role-policy-attach" {
-#   role       = data.aws_iam_role.apprunnerecr
-#   policy_arn = data.aws_iam_policy.apprunnerecr.arn
-# }
