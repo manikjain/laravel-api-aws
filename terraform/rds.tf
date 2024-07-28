ea156eba8b1164dea4dc3a330f20613480aa8377
manikjain <manik.jain@live.in>
2024-07-28 20:13:36 +0200

    fix port range in sg for rds

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -20,7 +20,7 @@ resource "aws_vpc_security_group_ingress_rule" "example" {
   security_group_id = aws_security_group.laravel.id
 
   cidr_ipv4   = "0.0.0.0/0"
-  from_port   = 0
+  from_port   = 3306
   ip_protocol = "tcp"
   to_port     = 3306
 }
